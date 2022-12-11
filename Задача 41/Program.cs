2faf93486b5a02a8ebdb2d1dcc8367f1cd13f3c9
ElenaEgorova <lenny_94@mail.ru>
2022-12-11 20:17:02 +0300

    Добавьте файлы проекта.

---
+++ Задача 41/Program.cs
@@ -0,0 +1,17 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Сколько чисел Вы хотите ввести?");
+int m = int.Parse(Console.ReadLine());
+int res = 0;
+Console.WriteLine("Вводите числа: ");
+
+for (int i = 0; i < m; i++)
+{
+    if (int.Parse(Console.ReadLine()) > 0)
+        {
+        res++;
+        }
+}
+
+
+Console.WriteLine("Ваши числа, которые больше 0: " + res);
---
+++ Задача 43/Program.cs
@@ -0,0 +1,42 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых,
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+//x = (b2 - b1) / (k1 - k2);
+
+//y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+
+
+
+Console.Write("Введите значение b1:  ");
+double b1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение k1:  ");
+double k1 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение b2:  ");
+double b2 = double.Parse(Console.ReadLine());
+
+Console.Write("Введите значение k2:  ");
+double k2 = double.Parse(Console.ReadLine());
+
+if ((k1 == k2) && (b1 == b2))
+
+{
+    Console.WriteLine();
+    Console.WriteLine("Прямые совпадают");
+}
+
+else if (k1 == k2)
+
+{
+    Console.WriteLine();
+    Console.WriteLine("Прямые параллельны");
+}
+
+else
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = (k1 * (b2 - b1)) / (k1 - k2) + b1;
+    Console.WriteLine();
+    Console.WriteLine("Точка пересечения двух заданных прямых: (" + x + ";" + y + ")");
+}
